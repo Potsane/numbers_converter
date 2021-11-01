a8d2f3749994971703842b6a87dd2ae34e86bc39
potsane <potsanemohale@gmail.com>
2021-11-01 11:17:25 +0200

    Add text to binary translations

--- app/src/main/java/com/app/rapidnumberconverter/ui/translation/TranslationViewModel.kt
+++ app/src/main/java/com/app/rapidnumberconverter/ui/translation/TranslationViewModel.kt
@@ -1,13 +1,28 @@
 package com.app.rapidnumberconverter.ui.translation
 
+import android.os.Build
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.app.rapidnumberconverter.ui.base.BaseRapidNumbersViewModel
+import com.app.rapidnumberconverter.utils.prettyBinary
+import com.app.rapidnumberconverter.utils.toBinary
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class  TranslationViewModel  @Inject constructor() : BaseRapidNumbersViewModel() {
+class TranslationViewModel @Inject constructor() : BaseRapidNumbersViewModel() {
+    private val _translatedText = MutableLiveData<String>()
+    val translatedText: LiveData<String> = _translatedText
+
+    val plainText = MutableLiveData("")
+
+    fun onTranslate() {
+        _translatedText.value = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            plainText.value?.toBinary()?.prettyBinary(8, " ")
+        else plainText.value?.toBinary()
+    }
 
     class Factory : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
