8e8e64ecc4f96fb688d33cb5388cbcae3ae8d0b0
potsane <potsanemohale@gmail.com>
2022-05-09 22:38:10 +0200

    Revamp about screen

--- app/src/main/java/com/app/rapidnumberconverter/ui/about/AboutViewModel.kt
+++ app/src/main/java/com/app/rapidnumberconverter/ui/about/AboutViewModel.kt
@@ -4,7 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.app.rapidnumberconverter.common.AboutCardItem
 import com.app.rapidnumberconverter.common.AboutCardItemClickListener
+import com.app.rapidnumberconverter.common.AboutCardItemType
 import com.app.rapidnumberconverter.ui.base.BaseRapidNumbersViewModel
+import com.app.rapidnumberconverter.utils.Constants.APP_URL
+import com.app.rapidnumberconverter.utils.Constants.OPEN_SOURCE_URL
+import com.app.rapidnumberconverter.utils.Constants.PAY_PAL_URL
+import com.app.rapidnumberconverter.utils.Constants.SHARE_APP_BODY
+import com.app.rapidnumberconverter.utils.Constants.SHARE_APP_TITLE
 import com.app.rapidnumberconverter.utils.aboutItems
 
 class AboutViewModel : BaseRapidNumbersViewModel(), AboutCardItemClickListener {
@@ -12,6 +18,20 @@ class AboutViewModel : BaseRapidNumbersViewModel(), AboutCardItemClickListener {
     val listItems = aboutItems
 
     override fun onPrimaryButtonClick(item: AboutCardItem) {
+        when (item.type) {
+            AboutCardItemType.RATE_US -> {
+                postUiCommand(LaunchExternalPage(APP_URL))
+            }
+            AboutCardItemType.SHARE_APP -> {
+                postUiCommand(ShareApp(SHARE_APP_TITLE, SHARE_APP_BODY))
+            }
+            AboutCardItemType.SUPPORT_APP -> {
+                postUiCommand(LaunchExternalPage(PAY_PAL_URL))
+            }
+            AboutCardItemType.LICENSE -> {
+                postUiCommand(LaunchExternalPage(OPEN_SOURCE_URL))
+            }
+        }
     }
 
     override fun onSecondaryButtonClick(item: AboutCardItem) {
---
+++ app/src/main/java/com/app/rapidnumberconverter/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.app.rapidnumberconverter.utils
+
+object Constants {
+
+    const val APP_URL = "market://details?id=online.whatsuptonight"
+    const val PAY_PAL_URL = "https://www.paypal.me/potsane"
+    const val OPEN_SOURCE_URL = "https://opensource.org/licenses/Apache-2.0"
+    const val SHARE_APP_TITLE = "Rapid numbers converter"
+    const val SHARE_APP_BODY =
+        "\n Hi there, I am using this amazing app. If you want to convert a number into binary, decimal, octal or hexadecimal system, you can install it from here.\n" +
+                "\n App link is:https://play.google.com/store/apps/details?id=online.whatsuptonight"
+}
